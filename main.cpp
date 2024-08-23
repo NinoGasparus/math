a36299fc7bc0cf2726384032bca48722e62e0376
Nino Gasparus <ngasparus@gmail.com>
2024-08-23 23:50:30 +0200

    can reliably parse ints  to 2^64-1

--- main.cpp
+++ main.cpp
@@ -5,12 +5,18 @@
 #include <iostream>
 #include <string>
 
+uint64_t betterPOW(int exp){
+  uint64_t base = 1;
+  for(int  i =  0; i < exp; i++){
+    base *= 10;
+  }
+  return base;
+}
 void showmem(uint64_t* memory, int size){
   for(int i =0; i < size; i++){
     std::cout<< std::bitset<64>(memory[i]) << "  " <<std::endl;
   }
 }
-void increment(uint64_t* memory, int exp, char number, int bufIndex);
 
 int main (int argc, char *argv[]) {
   
@@ -19,7 +25,7 @@ int main (int argc, char *argv[]) {
     memory[i] = 0;
   }
 
-  showmem(memory, 10);
+ // showmem(memory, 10);
 
   char* d10Word = new char[strlen(argv[1])];
 
@@ -27,31 +33,28 @@ int main (int argc, char *argv[]) {
     d10Word[i] = argv[1][i] - 48;
   }
 
-  for(int i =0; i < strlen(argv[1]); i++){
-    printf("%d",d10Word[i]);
-  }
   int asizee = strlen(argv[1]);
   printf("\n%d", asizee);
   int bufIndex = 0;
-  int exp  =asizee-1;
+  int exp  =  asizee-1;
+  printf("\n");
   
   for(int i = 0  ; i < asizee ; i++){
-    if(i%19  ==0 && i != 0){
-      bufIndex++;
-      exp = asizee-1;
-    }
-    increment(memory, exp, d10Word[i], bufIndex);
+    memory[bufIndex] += betterPOW( exp) * d10Word[i];
     exp--;
 
   }
-  printf("\n");
+    printf("\n");
   showmem(memory, 10);
+  
 
-
-
+  //uint64_t temp = 18446744073709551615UL;
+  //showmem(&temp, 1);
+  uint64_t argValue = std::strtoull(argv[1], nullptr, 10);
+  if (memory[0] == argValue) {
+        std::cout << "Passed" << std::endl;
+    } else {
+        std::cout << "Failed" << std::endl;
+    }
   return 0;
-}
-void increment(uint64_t* memory, int exp, char number, int bufIndex){
-  memory[bufIndex] += pow(10, exp) * number;
-
 }
