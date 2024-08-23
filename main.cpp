fc43bdeb1eb60eb4e5341cb0df3c517479f37f5e
Nino Gasparus <ngasparus@gmail.com>
2024-08-24 00:47:51 +0200

    works with up to 190 digits

--- main.cpp
+++ main.cpp
@@ -14,47 +14,62 @@ uint64_t betterPOW(int exp){
 }
 void showmem(uint64_t* memory, int size){
   for(int i =0; i < size; i++){
-    std::cout<< std::bitset<64>(memory[i]) << "  " <<std::endl;
+    std::cout<< std::bitset<64>(memory[i]) << "  => " <<memory[i] <<std::endl;
   }
+}
+
+void memCheck(char* StringNumber, uint64_t * memory){
+     
 }
 
 int main (int argc, char *argv[]) {
   
+  //working space
   uint64_t memory[10];
+  //clear the working space
   for(int i =0; i < sizeof(memory) / 8; i++){
     memory[i] = 0;
   }
 
- // showmem(memory, 10);
-
+  //pretty useles considering argv exsits however...
+  //used to stor intiger values of the  number 
   char* d10Word = new char[strlen(argv[1])];
-
+  //string->int conversion
   for(int i =0; i < strlen(argv[1]); i++){
     d10Word[i] = argv[1][i] - 48;
   }
-
-  int asizee = strlen(argv[1]);
-  printf("\n%d", asizee);
-  int bufIndex = 0;
-  int exp  =  asizee-1;
-  printf("\n");
   
-  for(int i = 0  ; i < asizee ; i++){
-    memory[bufIndex] += betterPOW( exp) * d10Word[i];
-    exp--;
-
+  //lenght of the number up to 2^64-1... for now  ofc
+  uint64_t asizee = strlen(argv[1]);
+  
+  std::cout << '\n' << asizee;
+  
+  //current buffer being writen to 
+  int bufIndex = 0;
+  //exponent 
+  int exp  = 0;
+  printf("\n");
+  for(int i = 0; i < (int)asizee/19+1; i++){
+    int chunkSize = 19;
+    if(i * 19 +19> asizee-1){
+      chunkSize = asizee - i*19;
+    }
+    for(int j = 0  ; j < chunkSize; j++){
+      memory[bufIndex] += betterPOW( exp) * d10Word[(asizee-1)-(i*19+j)];
+      exp++;
+    }
+    //printf("ran cycle of  %d itterations", chunkSize);
+    exp = 0;
+    bufIndex ++;
   }
-    printf("\n");
+
+  printf("\n");
   showmem(memory, 10);
   
-
-  //uint64_t temp = 18446744073709551615UL;
-  //showmem(&temp, 1);
-  uint64_t argValue = std::strtoull(argv[1], nullptr, 10);
-  if (memory[0] == argValue) {
-        std::cout << "Passed" << std::endl;
-    } else {
-        std::cout << "Failed" << std::endl;
-    }
+  //security check V2.0
+  memCheck(argv[1], memory);
   return 0;
 }
+
+
+
